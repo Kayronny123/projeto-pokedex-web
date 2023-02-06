4b6806dfd6fdaee6f2ae86d690821e8843d18c8e
Rogerio Kayronny Sales Pereira <rogerio.pereira@HMVCR.LOCAL>
2023-02-06 16:46:20 -0300

    adicionando imagens e redenrizando escolha de pokemons favoritos

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import { Searchbar } from '../Searchbar'
-
+import React,{useContext} from 'react'
+// import { Searchbar } from '../Searchbar'
+import heartfav from '../../assets/heart-fav.png'
+import FavoriteContext from '../../context/favoritesContext'
 export const NavBar = () => {
   const logoImg= "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
-  
+ 
+  const {favoritePokemons}= useContext(FavoriteContext);
+
   return (
     <nav>
     <div>
@@ -13,6 +16,10 @@ export const NavBar = () => {
       src={logoImg}
       className='navbar-img'
       />
+    </div>
+    <div>
+      {favoritePokemons.length}
+      <img alt='favorito' src={heartfav} style={{width: '25px', height: '25px',}}  />
     </div>
     </nav>
   )
