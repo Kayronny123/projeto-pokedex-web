1eebb621ea4d0b702ee04f05db89f4c04c477cf3
Rogerio Kayronny Sales Pereira <rogerio.pereira@HMVCR.LOCAL>
2023-02-06 16:48:06 -0300

    adicionando funções por props do components Pagination

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import Pokemon from './Pokemon'
-
+import Pagination from './Pagination'
 function Pokedex(props) {
-    const { pokemons, loading } = props
+    const { pokemons, loading, page, totalPages, setPage } = props
+    const onLeftClickHandler = ()=>{
+        if (page > 0 ){
+            setPage(page-1)
+        }
+    }
+    const onRightClickHandler = ()=>{
+        if(page+1 !== totalPages ){
+            setPage(page+1)
+        }
+    }
+
     return (
         <div>
             <div className='pokedex-header'>
                 <h1>Pokedex</h1>
-                <div>Paginação</div>
+               <Pagination
+               setPage={setPage}
+               page={page+1}
+               totalPages={totalPages} 
+               onLeftClick={onLeftClickHandler}
+               onRightClick={onRightClickHandler}
+               />
             </div>
             {loading ? (
             <div>Carregando...</div>
