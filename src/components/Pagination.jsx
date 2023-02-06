8ae698f4ff5af97cc2bb8dd52dd650c30c2b0018
Rogerio Kayronny Sales Pereira <rogerio.pereira@HMVCR.LOCAL>
2023-02-06 16:47:19 -0300

    criando estrutura do components de paginação

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import left from '../assets/seta-esquerda.png'
+import right from '../assets/seta-direita.png'
 
 function Pagination(props) {
   const {page, totalPages, onLeftClick, onRightClick}= props
     return (
     <div className='pagination-container'>
-        <button onClick={onLeftClick}></button>
-        <div></div>
-        <button onClick={onRightClick}></button>
+        <button onClick={onLeftClick}><img alt='esquerda' src={left}  style={{width: '30px', height: '20px',}}/> 
+        </button>
+        <div>{page} de {totalPages} </div>
+        <button onClick={onRightClick}><img alt='direita' src={right}  style={{width: '30px', height: '20px' }}/> </button>
     </div>
   )
 }
