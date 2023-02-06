9e207a9c41ad53993073440f86fb8c59d0be5a66
Rogerio Kayronny Sales Pereira <rogerio.pereira@HMVCR.LOCAL>
2023-02-06 16:41:20 -0300

    adicionando hook usecontext e usando suas propiedades na paginação

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,29 @@ import { Searchbar } from './components/Searchbar'
 import { getPokemonData, getPokemons, searchPokemon } from './api'
 import axios from 'axios'
 import Pokedex from './components/Pokedex'
+import { FavoritePovider } from './context/favoritesContext'
+
+const favoriteKey = 'f'
 function App() {
+  const [page, setPage] = useState(0)
+  const [totalPages, setTotalPages] = useState(0)
   const [loading, setLoading] = useState(false)
   const [pokemons, setPokemons] = useState([])
+  const [favorites, setFavorites] = useState([])
+  const itensPerPage = 25
+
  const fetchPokemons = async()=>{
     try {
       setLoading(true)
-      const data = await getPokemons()
+      const data = await getPokemons(itensPerPage, itensPerPage * page)
+
       const promises = data.results.map(async(pokemon)=>{
         return await getPokemonData(pokemon.url)
       })
       const results = await Promise.all(promises)
       setPokemons(results)
       setLoading(false)
+      setTotalPages(Math.ceil(data.count / itensPerPage) )
     } catch (error) {
       console.log('fetchpokemons', error)
     }  
@@ -26,15 +36,41 @@ function App() {
   useEffect(() => {
     console.log('carregou fetch')
     fetchPokemons();
+  }, [page]);
+  
+  const loadFavoritePokemons = ()=>{
+    const pokemons = JSON.parse(window.localStorage.getItem(favoriteKey)) || []
+    setFavorites(pokemons)
+  }
+  useEffect(() => {
+    console.log('carregou loadFavorite')
+    loadFavoritePokemons()
   }, []);
   
+  const updateFavoritesPokemons = (name)=>{
+    const updateFavorites = [...favorites]
+    const favoriteIndex= favorites.indexOf(name)
+    if (favoriteIndex >= 0) {
+      updateFavorites.slice(favoriteIndex, 1)
+    } else {
+      updateFavorites.push(name)
+    }
+    window.localStorage.setItem(favoriteKey, JSON.stringify(updateFavorites))
+    setFavorites(updateFavorites)
+  }
 
   return (
+<FavoritePovider value={{favoritePokemons: favorites,
+updateFavoritesPokemons: updateFavoritesPokemons
+}}>
+
     <div>
   <NavBar/>
   <Searchbar />
-  <Pokedex  pokemons={pokemons} loading={loading}  />
+  <Pokedex  pokemons={pokemons} loading={loading} page={page} totalPages={totalPages} setPage={setPage} />
+  
     </div>
+    </FavoritePovider>
   )
 }
 
